3e82e14a60afecf447da6c7fb856c08320d1aed8
Chamodi Batuwanthudawe <chamodibatuwanthudawe001@gmail.com>
2025-05-08 14:51:38 +0530

    featured jobs started

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,7 +11,23 @@ class HomeController extends Controller
     // this method will show our home page
     public function index(){
 
-        return view('front.home');
+      $categories = Category::where('status' , 1)->orderBy('name' , 'ASC')->take(8)->get();
+
+      $featuredJobs = Job::where('status',1)
+      ->orderBy('created_at' , 'DESC')
+      ->where('isFeatured', 1)->take(6)->get();
+
+      $latestJobs = Job::where('status',1)
+      ->orderBy('created_at' , 'DESC')
+      ->take(6)->get();
+
+
+        return view('front.home', [
+            'categories' => $categories,
+            'featuredJobs' => $featuredJobs,
+            'latestJobs' => $latestJobs,
+           
+        ]);
     }
      
 }
