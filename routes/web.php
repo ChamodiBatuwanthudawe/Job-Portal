f74fb3d439fc6e7a82ee53acd747183bc635c896
Chamodi Batuwanthudawe <chamodibatuwanthudawe001@gmail.com>
2025-05-08 16:53:52 +0530

    Jobs Page Completed

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\JobsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-
+Route::get('/jobs', [JobsController::class, 'index'])->name('jobs');
 Route::group(['prefix' => 'account'], function() {
     // Guest routes
     Route::group(['middleware' => 'guest'], function() {
